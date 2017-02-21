d53824610cd6d7b57973cf415aa9fbf5e04e8487
viktorreglin <viktor.reglin@gmail.com>
2017-02-21 23:19:59 +0100

    load_db can now unzip, but lacks folder structure

--- src/load_db/load_db.c
+++ src/load_db/load_db.c
@@ -1,13 +1,21 @@
-// Downloads the cards database.
+// Downloads and unpacks the cards database.
 
 #include <stdio.h>
+#include <unistd.h> // For chdir().
 
-#define BUFFLEN 1000
+#define STRLEN 1000
 
 
 int main(){
-   char system_command[BUFFLEN] = "wget https://mtgjson.com/json/AllSets.json.zip";
+   char dir[STRLEN] = "/home/viktor/Documents/MEGAsync/Anderes/Computer/Programming/viktor/C/Magic/git/magic/data/";
+   char remove_command[STRLEN] = "rm AllSets.json AllSets.json.zip";
+   char wget_command[STRLEN] = "wget https://mtgjson.com/json/AllSets.json.zip";
+   char unzip_command[STRLEN] = "unzip AllSets.json.zip";
 
-   system( system_command );
+   chdir( dir );
+   system( remove_command );
+   system( wget_command );
+   system( unzip_command );
+
    return 0;
 }
