64bad0c8eaa80702ac8aa7cc259669913c29511b
viktorreglin <viktor.reglin@gmail.com>
2017-11-19 20:16:08 +0100

    Manakosten von sample.mc können ermittelt werden

--- src/statistics/statistics.c
+++ src/statistics/statistics.c
@@ -1,5 +1,12 @@
 // Berechnet Statistiken zu einem Deck.
 
+/* HINZUFÜGEN
+ * Header-Datei mit enum für die Farben?
+ * Zählen der Kartentypen
+ * Zählen der Raritäten: Karten nacheinander durchgehen. Es gibt die Möglichkeiten Common, Uncommon, Rare, Mythic Rare, Basic Land, Special. Spaltenüberschrift ist "rarity".
+ * 
+ */
+
 
 #include <stdio.h>
 #include <string.h>
@@ -21,13 +28,27 @@ int occurence( char search_for, char * search_in ){
 }
 
 
-// Gibt von einer Karte die Manakosten zurück.
-void manacosts( char * cardcosts, int * occurences ){
+// Gibt von einer Karte die Anzahlen der Manasymbole zurück.
+void manacosts( char * cardcosts, int * symbol_counts ){
    char colors[6] = {'B', 'C', 'G', 'U', 'R', 'W'};
    int i;
    
    for( i = 0; i < ( sizeof( colors ) / sizeof( char ) ); i++ ){
-      occurences[i] += occurence( colors[i], cardcosts );
+      symbol_counts[i] += occurence( colors[i], cardcosts );
+   }
+}
+
+
+// Gibt von einer Karte die Rarität zurück.
+void rarity( char * rarity_string, int * rarity_counts ){
+   char *rarities[6] = { "Common", "Uncommon", "Rare", "Mythic Rare", "Special", "Basic Land" };
+   int i;
+   
+   for( i = 0; i < ( sizeof( rarities ) / sizeof( rarities[0] ) ); i++ ){
+      if( strstr( rarity_string, rarities[i] ) && rarity_string[0] == rarities[i][0] ){
+         rarity_counts[i]++;
+         break;
+      }
    }
 }
 
@@ -40,13 +61,17 @@ char char_n( FILE * fp, int n, char * buf ){
 }
 
 
-// Findet in einer Datei die erste Zeile, die mit dem angegebenen Zeichen beginnt. Gibt die Zeile as String zurück.
+// Findet in einer Datei die nächste Zeile, die mit dem angegebenen Zeichen beginnt. Gibt die Zeile as String zurück.
 long row_num( FILE * fp, char symbol, char * buf, long pos_in_file ){
    
    fseek( fp, pos_in_file, SEEK_SET ); // Starte bei der letzten Position, nicht am Anfang.
    
    for(;;){
-      if( fgets( buf, STRINGSIZE, fp ) == NULL || buf[0] == symbol ) break;
+      if( fgets( buf, STRINGSIZE, fp ) == NULL ) {
+         memset( buf, 0, STRINGSIZE ); // Setzt alle Elemente von buf auf "0". Damit nicht die letzte Zeile des Decks ausgewertet wird, egal ob sie mit C anfängt.
+         break;
+      }
+      if( buf[0] == symbol ) break;
    }
    
    return ftell( fp ); // Position in der Datei merken.
@@ -90,7 +115,7 @@ void nth_element( char * search_in, char delimiter, int n, char * substring ){
    int start, end;
    
    start = nth_pos( search_in, delimiter, n ) + 1;
-   end = nth_pos( search_in, delimiter, n + 1 ) - 1;
+   end = nth_pos( search_in, delimiter, n + 1 );
    
    sprintf( substring, "%.*s", end - start, search_in + start );
 }
@@ -108,44 +133,83 @@ static long filelength( FILE * fp )
 }
 
 
+// Füllt einen Array mit den Anzahlen der Manasymbole eines Decks.
+void manasymbols( FILE * fp, long filelength, int * symbol_counts, char card_row, char * buf, char * substring, char delimiter, int del_count ){
+   long  pos_in_file = 0;
+   int   i;
+
+   for(;;){
+      if( pos_in_file == filelength ) break; // Aufhören sobald das Ende der Datei erreicht wurde.
+      pos_in_file = row_num( fp, card_row, buf, pos_in_file ); // Packt die nächste C-Zeile in ein Array.
+      nth_element( buf, delimiter, del_count, substring ); // Packt den Teilstring der Manakosten in ein Array.
+      
+      manacosts( substring, symbol_counts ); // Zählt die Manasymbole pro Farbe.
+   }
+}
+
+
+// Füllt einen Array mit den Anzahlen der Kartenraritäten eines Decks.
+void rarities( FILE * fp, long filelength, int * rarity_counts, char card_row, char * buf, char * substring, char delimiter, int del_count ){
+   long  pos_in_file = 0;
+   int   i;
+
+   for(;;){
+      if( pos_in_file == filelength ) break; // Aufhören sobald das Ende der Datei erreicht wurde.
+      pos_in_file = row_num( fp, card_row, buf, pos_in_file ); // Packt die nächste C-Zeile in ein Array.
+      nth_element( buf, delimiter, del_count, substring ); // Packt den Teilstring der Rarität in ein Array.
+      
+      rarity( substring, rarity_counts ); // Zählt die Manasymbole pro Farbe.
+   }
+}
+
 
 // Main.
 void main(){
-   const int del_pos = 1; // Trennzeichen ist das zweite Zeichen in einer Datei.
-   char header_row = 'H'; // Überschriften stehen in der Zeile, die mit H beginnt.
-   char card_row = 'C'; // Kartenzeilen beginnen mit C.
-   char * attribute = "manaCost";
+   int   del_pos = 1; // Trennzeichen ist das zweite Zeichen in einer Datei.
+   int   del_count;
+   int   symbol_counts[6] = { 0 }; // 6 Farben, inklusive "farblos".
+   int   rarity_counts[6] = { 0 }; // 6 Raritäten: "Common", "Uncommon", "Rare", "Mythic Rare", "Special", "Basic Land"
+   int   i;
    
-   FILE * fp;
-   char buf[STRINGSIZE];
-   char substring[STRINGSIZE];
-   char delimiter;
-   int del_count;
-   int occurences[6] = {0,0,0,0,0,0}; // 6 Farben, inklusive "farblos".
-   int i;
-   long pos_in_file = 0;
-   long length;
+   char  header_row = 'H'; // Überschriften stehen in der Zeile, die mit H beginnt.
+   char  card_row = 'C'; // Kartenzeilen beginnen mit C.
+   char  buf[STRINGSIZE];
+   char  substring[STRINGSIZE];
+   char  delimiter;
+   char  *mana_heading = "manaCost";
+   char  *rarity_heading = "rarity";
    
+   FILE  *fp;
+   long  length;
+    
    fp = fopen( FILENAME, "r" );
+   length = filelength( fp );
    delimiter = char_n( fp, del_pos, buf ); // Bestimmt das Trennzeichen.
    row_num( fp, header_row, buf, 0 ); // Packt die H-Zeile in ein Array.
-   del_count = nth_delimiter( attribute, buf, delimiter ); //Gibt an, das wievielte Element manaCost ist.
    
-   length = filelength( fp );
    
-   for(;;){
-      if( pos_in_file == length ) break; // Aufhören sobald das Ende der Datei erreicht wurde.
-      pos_in_file = row_num( fp, card_row, buf, pos_in_file ); // Packt die erste T-Zeile in ein Array.
-      nth_element( buf, delimiter, del_count, substring ); // Packt den Teilstring der Manakosten in ein Array.
-      
-      manacosts( substring, occurences ); // Zählt die Manakosten pro Farbe.
-   }
+   // Manasymbole:   
+   del_count = nth_delimiter( mana_heading, buf, delimiter ); //Gibt an, das wievielte Element manaCost ist.
+   manasymbols( fp, length, symbol_counts, card_row, buf, substring, delimiter, del_count );
    
-   printf( "\nManacosts:\n" );
-   for( i = 0; i < ( sizeof( occurences ) / sizeof( int ) ); i++ ){
-      printf( " %d ", occurences[i] );
+   printf( "\nManasymbols:\n" );
+   for( i = 0; i < ( sizeof( symbol_counts ) / sizeof( int ) ); i++ ){
+      printf( " %d ", symbol_counts[i] );
    }
    printf( "\n" );
+   
+   
+   // Raritäten:
+   row_num( fp, header_row, buf, 0 ); // Setzt buf zurück auf die H-Zeile.
+   del_count = nth_delimiter( rarity_heading, buf, delimiter ); //Gibt an, das wievielte Element rarity ist.
+   rarities( fp, length, rarity_counts, card_row, buf, substring, delimiter, del_count );
+   
+   printf( "\nRarities:\n" );
+   for( i = 0; i < ( sizeof( rarity_counts ) / sizeof( int ) ); i++ ){
+      printf( " %d ", rarity_counts[i] );
+   }
+   printf( "\n" );
+   
    
    fclose( fp );
    
